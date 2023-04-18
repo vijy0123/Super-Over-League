70899295dcd81b0f41581c1f313138371b68e3c2
vijy0123 <vijaydyagala0123@gmail.com>
2023-04-18 08:54:02 +0000

    Super Over League Solution

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="imgContainer">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
